ee89ec95aa809eec6fc35a77ff010a6137ec1c35
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2020-12-13 11:03:24 +0100

    Better heading example with scss module

--- src/components/primitives/heading/Heading.tsx
+++ src/components/primitives/heading/Heading.tsx
@@ -1,17 +1,32 @@
 import React, { ReactNode } from 'react';
+import createClassName from 'classnames';
+import classnames from './Heading.module.scss';
 
 export enum HeadingTag {
     H1 = 'h1',
     H2 = 'h2',
 }
 
+export enum HeadingStyle {
+    Primary,
+    Secondary,
+}
+
 type Props = {
     tag: HeadingTag;
     children: ReactNode;
+    style?: HeadingStyle;
 };
 
-const Heading: React.FC<Props> = ({ children, tag }) => {
-    const className = 'heading';
+const Heading: React.FC<Props> = ({
+    children,
+    tag,
+    style = HeadingStyle.Primary,
+}) => {
+    const className = createClassName(classnames.heading, {
+        [classnames.isStylePrimary]: style === HeadingStyle.Primary,
+        [classnames.isStyleSecondary]: style === HeadingStyle.Secondary,
+    });
 
     return React.createElement(tag, { className }, children);
 };
